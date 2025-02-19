a805dbbd16bd167615280d14b6dd0bd4c27efdc4
YATTOURA <mohamedbakaryyattoura@gmail.com>
2025-02-19 12:17:43 +0100

    correction

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Experiences from "./components/Experiences";
 import Projects from "./components/Projects";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
